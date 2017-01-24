8f83335175801f4ebd054229341722b55259ba1a
Pierre <pierre.haller@telecomnancy.net>
2017-01-24 09:50:06 +0100

    timerTask ok, but wont cancel

--- app/src/main/java/eu/telecomnancy/tncyiot/MainService.java
+++ app/src/main/java/eu/telecomnancy/tncyiot/MainService.java
@@ -20,66 +20,25 @@ package eu.telecomnancy.tncyiot;
  * helper methods.
  */
 public class MainService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
+
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    public static final String ACTION_FOO = "eu.telecomnancy.tncyiot.action.FOO";
+    public static final String ACTION_ON = "eu.telecomnancy.tncyiot.action.ON";
+    public static final String ACTION_OFF = "eu.telecomnancy.tncyiot.action.OFF";
 
     // TODO: Rename parameters
     public static final String EXTRA_PARAM1 = "eu.telecomnancy.tncyiot.extra.PARAM1";
     public static final String EXTRA_PARAM2 = "eu.telecomnancy.tncyiot.extra.PARAM2";
 
-    //
-    TimerTask timerTask;
+
+    private static TimerTask timerTask;
+    private static Timer myTimer;
+
     final Handler handler = new Handler();
-
-
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, MainService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
 
     public MainService() {
         super("MainService");
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        Log.d("IntentService",
-                "onHandleIntent called"
-        );
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            }
-        }
-    }
-
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        Log.d("IntentService",
-                "handleActionFoo called"
-        );
         timerTask = new TimerTask() {
             public void run() {
-
                 //use a handler to run a toast that shows the current timestamp
                 handler.post(new Runnable() {
                     public void run() {
@@ -96,9 +55,44 @@ public class MainService extends IntentService {
                 });
             }
         };
-        timerTask.run();
+        myTimer = new Timer();
     }
 
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("IntentService",
+                "onHandleIntent called"
+        );
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_ON.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionOn(param1, param2);
+            } else if (ACTION_OFF.equals(action)){
+                handleActionOff();
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionOn(String param1, String param2) {
+        Log.d("IntentService",
+                "handleActionOn called"
+        );
+        myTimer.scheduleAtFixedRate(timerTask, 0, 5000);
+    }
+
+    private void handleActionOff(){
+        Log.d("IntentService",
+                "handleActionOff called"
+        );
+        timerTask.cancel();
+        myTimer.purge();
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
